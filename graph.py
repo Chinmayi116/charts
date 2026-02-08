130eefdf580f8cc9e995554e56093b4a3e527115
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-02-08 20:01:50 +0530

    Add simple line graph plotting with matplotlib

---
+++ graph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Data for the graph
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create the graph
+plt.figure()
+plt.plot(x, y)
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.title("Simple Line Graph in Python")
+plt.show()
